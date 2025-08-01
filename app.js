39eafd90396f5f8473ff946b00c31bdad3873ffd
Danett arana <danaranag@gmail.com>
2025-07-31 21:39:36 -0700

    Declarar funciones principales para la lógica del proyecto

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Aplicación que permita a los usuarios ingresar nombres de amigos en una lista para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto".
+
+
+let amigos = []; //lista de amigos
+
+// Agrega un amigo a la lista
+function agregarAmigo(){}
+
+//Raliza el sorteo
+function sortearAmigo() {}
+
+//Muestra la lista de amigos
+function mostrarLista() {}
+
+//Valida la entrada de nombres
+function validarEntrada() {}
